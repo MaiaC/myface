a62db779207403ec97ce3cab487b701af9d0ab1a
Maia Coghlan <maiaccog@gmail.com>
2019-03-17 16:20:26 +1100

    remove mailer

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   has_many :likes
   has_many :comments
 
-  after_create :send_welcome_mail
+  # after_create :send_welcome_mail
 
   def friends
     friends = friendships.map { |f| f.friend if f.accepted }.compact
